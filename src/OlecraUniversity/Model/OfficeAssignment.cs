9b2115d5ebbb6b686d782a95370a55f57f996039
Joe Arcelo <josepharcelo@gmail.com>
2016-08-30 23:48:20 -0500

    Add person and student model

---
+++ src/OlecraUniversity/Model/OfficeAssignment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniv.Models
+{
+    public class OfficeAssignment
+    {
+        [Key] // Use this attribute to explicitly tell EF that this is the primary key.
+        [ForeignKey("Instructor")] // Applied to this dependent class to establish a relationship.
+        public int InstructorID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        // Navigation property
+        // Instructor and OfficeAssignment have one-to-zero-or-one relationship
+        public virtual Instructor Instructor { get; set; }
+    }
+}
---
+++ src/OlecraUniversity/Model/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniv.Models
+{
+    public class Student : Person
+    {
+        // Primary Key
+        //public int ID { get; set; }
+
+        //[Required]
+        //[Display(Name = "Last Name")]
+        //[StringLength(50, ErrorMessage = "{0} cannot be longer than {1} characters")]
+        //public string LastName { get; set; }
+
+        //[Required]
+        //[Display(Name = "First Name")]
+        //[StringLength(50, ErrorMessage = "{0} cannot be longer than {1} characters")]
+        //[Column("FirstName")]
+        //public string FirstMidName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date, ErrorMessage = "{0} must be in MM/DD/YYY format" )]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
+        public DateTime EnrollmentDate { get; set; }
+
+        // Navigation Property 
+        // There's one to many relationship between Student and Enrollment entities.
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        //[Display(Name = "Full Name")]
+        //public string  FullName {
+        //    get
+        //    {
+        //        return LastName + ", " + FirstMidName;
+        //    }
+        //}
+    }
+
+}
