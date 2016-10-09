2aba55b8a34fa521b258efde825aab50dbf80448
Joe Arcelo <josepharcelo@gmail.com>
2016-10-09 13:39:58 -0500

    Add Entity Framework Core to project.json     -Add Microsoft.EntityFrameworkCore.SqlServer v1.0.1     -Add Microsot.EntityFrameworkCore.Tools preview

---
+++ src/OlecraUniversity/Data/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using OlecraUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace OlecraUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
--- src/OlecraUniversity/Model/Enrollment.cs
+++ src/OlecraUniversity/Model/Enrollment.cs
@@ -16,15 +16,13 @@ namespace OlecraUniversity.Models
         public int CourseID { get; set; }
         public int StudentID { get; set; }
 
-        // Question mark means that this is nullable
-        // Null means it is not known or hasn't been assigned yet
         [DisplayFormat(NullDisplayText = "No Grade")]
         public Grade? Grade { get; set; } 
 
         // Navigation properties
-        // An enrollment entity is associated with one course and student entities
-        public virtual Course Course { get; set; }
-        public virtual Student Student { get; set; }
+        // An enrollment entity is associated with ONE course and student entities
+        public Course Course { get; set; }
+        public Student Student { get; set; }
 
     }
 }
