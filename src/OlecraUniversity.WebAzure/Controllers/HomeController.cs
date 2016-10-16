02ca4de325653b46c698ead25a6ab682eea2762b
Joe Arcelo <josepharcelo@gmail.com>
2016-10-16 15:50:50 -0500

    Fix reference error and add custom constructor in HomeController

--- src/OlecraUniversity.WebAzure/Controllers/HomeController.cs
+++ src/OlecraUniversity.WebAzure/Controllers/HomeController.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OlecraUniversity.WebAzure.Models.SchoolViewModels;
+using OlecraUniversity.WebAzure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace OlecraUniversity.WebAzure.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
